c3c76c11417d0a51eaaa8636d0ed7a4b467d6e5b
denysyatsenko228 <dayyz@bk.ru>
2022-02-23 16:38:08 +0200

    Full e-learning project

---
+++ educa/educa/settings/pro.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Denys Y', 'dayyz2000@gmail.com')
+)
+
+ALLOWED_HOSTS = ['*']
+
+# add postgresql settings
+DATABASES = {
+    'default': {
+
+    }
+}
